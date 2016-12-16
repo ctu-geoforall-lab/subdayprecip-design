85f87511cee6b32603de9d6ef9704c44727edf9b
Martin Landa <landa.martin@gmail.com>
2016-12-16 12:21:44 +0100

    +rsync in publish.sh

--- webapp/publish.sh
+++ webapp/publish.sh
@@ -6,13 +6,15 @@ if test -z $1 ; then
 fi
 user="$1"
 if test -z $2 ; then
-    echo "specify user"
+    echo "specify host"
     exit 1
 fi
 host="$2"
 
 basename="webapp"
 
+cd ../qgis
+rsync -av --delete data.sqlite webapp.qgs $user@$host:/opt/subdayprecip-design/webapp/publish/rain/
 for f in `ls -t ${basename}_* | head -2`; do
     scp $f $user@$host:/opt/subdayprecip-design/webapp/publish/rain/
 done
