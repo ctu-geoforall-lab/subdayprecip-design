c7d87d1caf81a5983ca41f9fd0f642512fb0e762
Martin Landa <landa.martin@gmail.com>
2017-05-08 13:05:28 +0200

    rewrite to pywps4 (shp)

--- wps/base/subdayprecip.py
+++ wps/base/subdayprecip.py
@@ -24,94 +24,111 @@ sys.path.append(os.path.join(os.environ["GISBASE"], "etc", "python"))
 logging.info(sys.path)
 from grass.pygrass.modules import Module
 from grass.exceptions import CalledModuleError
-from pywps.Process import WPSProcess
 
-class SubDayPrecipProcess(WPSProcess):
-     def __init__(self, identifier, description, location='subdayprecip', skip=[]):
-          WPSProcess.__init__(self,
-                              identifier=identifier,
-                              version="0.1",
-                              title="Návrhová srážka pro zvolenou lokalitu. " + description,
-                              abstract="Počítá návrhovou srážku pro zvolenou lokalitu s využitím nástroje GRASS GIS r.subdayprecip.design. "
-                              "Více informací na http://rain.fsv.cvut.cz/nastroje/r.subdayprecip.design",
-                              grassLocation=location, storeSupported = True, statusSupported = True)
+from pywps import Process, ComplexInput, LiteralInput, Format
 
+class SubDayPrecipProcess(Process):
+     def __init__(self, identifier, description, location='/opt/grassdata/subdayprecip', skip=[],
+                  inputs=[], outputs=[]):
           if 'input' not in skip:
-               self.input = self.addComplexInput(identifier = "input",
-                                                 title = "Vstupní bodová nebo polygonová vektorová data",
-                                                 formats = [ {"mimeType":"text/xml",
-                                                              "encoding":"utf-8",
-                                                              "schema":"http://schemas.opengis.net/gml/3.2.1/gml.xsd"} ],
-                                                 minOccurs=0)
-          
-          self.return_period = self.addLiteralInput(identifier = "return_period",
-                                                    title = "Doby opakování",
-                                                    type = types.StringType,
-                                                    default = "N2,N5,N10,N20,N50,N100")
+               inputs.append(ComplexInput(
+                    identifier="input",
+                    title="Vstupni bodová nebo polygonova vektorova data",
+                    supported_formats=[Format('application/gml+xml'),
+                                       Format('application/zip; charset=binary')])
+               )
 
-          self.rainlength_value = None
+          inputs.append(LiteralInput(
+               identifier="return_period",
+               title="Doby opakovani",
+               data_type='string',
+               default="N2,N5,N10,N20,N50,N100")
+          )
           if 'rainlength' not in skip:
-               self.rainlength = self.addLiteralInput(identifier = "rainlength",
-                                                      title = "Délka srážky v minutách",
-                                                      type = types.IntType)
+               inputs.append(LiteralInput(
+                    identifier="rainlength",
+                    title="Delka srazky v minutach",
+                    data_type='integer')
+               )
 
+          super(SubDayPrecipProcess, self).__init__(
+               self._handler,
+               identifier=identifier,
+               version="0.1",
+               title="Navrhova srazka pro zvolenou lokalitu. " + description,
+               abstract="Pocita navrhovou srazku pro zvolenou lokalitu s vyuzitim nastroje GRASS GIS r.subdayprecip.design. Vice informaci na http://rain.fsv.cvut.cz/nastroje/r.subdayprecip.design",
+               inputs=inputs,
+               outputs=outputs,
+               grass_location=location,
+               store_supported=True,
+               status_supported=True)
+          
+          self.rainlength_value = None
           self.output = None # to be defined by descendant
           self.output_dir = None
           
           os.environ['GRASS_SKIP_MAPSET_OWNER_CHECK'] = '1'
           os.environ['HOME'] = '/tmp' # needed by G_home()
           
-     def __del__(self):
-          if self.output_dir:
-               shutil.rmtree(self.output_dir)
+     # def __del__(self):
+     #      if self.output_dir:
+     #           shutil.rmtree(self.output_dir)
 
-     def execute(self):
+     def _handler(self, request, response):
           if not self.rainlength_value:
-               self.rainlength_value = self.rainlength.getValue()
-          if self.input:
-               self.map_name = self.import_data()
-          else:
-               self.map_name = self.copy()
+               self.rainlength_value = request.inputs['rainlength'][0].data
+          # if 'input' in request.inputs.keys():
+          #      self.map_name = self.import_data()
+          # else:
+          #      self.map_name = self.copy()
+          self.map_name = self.import_data(request.inputs['input'][0].data)
+          
+          if 'keycolumn' in request.inputs.keys():
+               self.check_keycolumn(request.inputs['keycolumn'][0].data)
 
-          if hasattr(self, 'keycolumn'):
-               self.check_keycolumn(self.keycolumn.getValue())
+          self.rasters = request.inputs['return_period'][0].data.split(',')
 
-          self.rasters = self.return_period.getValue().split(',')
-
-          self.output_dir = os.path.join('/tmp', '{}_{}'.format(self.map_name, os.getpid()))
+          self.output_dir = os.path.join('/tmp', '{}_{}'.format(
+               self.map_name, os.getpid())
+          )
           os.mkdir(self.output_dir)
 
           Module('g.region', raster=self.rasters[0])
           logging.debug("Subday computation started")
           start = time.time()
           Module('r.subdayprecip.design',
-                 map=self.map_name, return_period=self.rasters, rainlength=self.rainlength_value)
+                 map=self.map_name, return_period=self.rasters,
+                 rainlength=self.rainlength_value
+          )
           logging.info("Subday computation finished: {} sec".format(time.time() - start))
-          logging.info("{}".format(Module('v.info', flags='c', map=self.map_name, stdout_=PIPE).outputs.stdout))
-          self.export()
-         
+          logging.info("{}".format(Module(
+               'v.info', flags='c', map=self.map_name, stdout_=PIPE).outputs.stdout)
+          )
+          response.outputs['output'].file = self.export()
+
+          return response
+     
      def check_keycolumn(self, keycol):
           # check if key columns exists
-          map_cols = Module('db.columns', table=self.map_name, stdout_=PIPE).outputs.stdout.splitlines()
+          map_cols = Module('db.columns',
+                            table=self.map_name, stdout_=PIPE).outputs.stdout.splitlines()
           if keycol not in map_cols:
-               raise StandardError("Key column ({}) not found in input attribute table ({})".format(
-                         keycol, ','.join(map_cols)
-               ))
+               raise StandardError(
+                    "Key column ({}) not found in input attribute table ({})".format(
+                    keycol, ','.join(map_cols))
+               )
           
-     def import_data(self, link_only=False):
+     def import_data(self, input_data, link_only=False):
           map_name = 'subdayprecip_output'
-          input_data = self.input.getValue()
 
-          # guess mine type
-          m = magic.open(magic.MAGIC_MIME)
-          m.load()
-          mime = m.file(input_data)
-          logging.debug('Input ({}) mine type: {}'.format(input_data, mime))
-          if 'gzip' in mime:
-               input_data = '/vsigzip/' + input_data
-          elif 'zip' in mime:
-               os.rename(input_data, input_data + '.zip') # GDAL requires zip extension
-               input_data = '/vsizip/' + input_data + '.zip'
+          prefix = '/'
+          if input_data.endswith('gzip'):
+               prefix += 'vsigzip/' 
+          elif input_data.endswith('zip'):
+               prefix += 'vsizip/'
+          if input_data.startswith('http'):
+               prefix += 'vsicurl/'
+          input_data = prefix + input_data
 
           # link or import ?
           module_in_args = {}
@@ -130,19 +147,24 @@ class SubDayPrecipProcess(WPSProcess):
                Module(module_in,
                       output=map_name,
                       overwrite=True,
-                      **module_in_args)
+                      **module_in_args
+               )
           except CalledModuleError as e:
                raise Exception("Unable to import input vector data: {}".format(e))
           
-          logging.info("Input data imported ({}): {} sec".format(module_in, time.time() - start))
+          logging.info("Input data imported ({}): {} sec".format(
+               module_in, time.time() - start)
+          )
           
           return map_name
      
-     def copy(self):
-          map_name = self.input.getValue()
-          Module('g.copy', vector=['{}@PERMANENT'.format(map_name),map_name],
-                 overwrite=True)
-          return map_name
+     # def copy(self):
+     #      map_name = request.inputs['input'][0].data
+     #      Module('g.copy', vector=['{}@PERMANENT'.format(map_name),map_name],
+     #             overwrite=True
+     #      )
+
+     #      return map_name
      
      def export(self):
           pass
