0ad16fe1d180c8dd43407ee637dc5bbadcd56a7b
Martin Landa <landa.martin@gmail.com>
2017-06-07 09:12:49 +0200

    fix identifier in autotest

--- wps/autotest.sh
+++ wps/autotest.sh
@@ -10,11 +10,11 @@ STYP="1,2"
 cd /tmp
 
 echo "**************************************************************"
-echo "* subdayprecip-design-shp"
+echo "* d-rain-shp"
 echo "**************************************************************"
 
 file=`curl \
-"https://rain1.fsv.cvut.cz:4433/services/wps?service=wps&version=1.0.0&request=Execute&identifier=subdayprecip-design-shp&datainputs=\[input=${DATA};return_period=${RP};rainlength=${RL}\]" | \
+"https://rain1.fsv.cvut.cz:4433/services/wps?service=wps&version=1.0.0&request=Execute&identifier=d-rain-shp&datainputs=input=${DATA};return_period=${RP};rainlength=${RL}" | \
 grep '\<wps:Reference' | cut -d'"' -f2`
 
 wget -q $file
@@ -22,11 +22,11 @@ echo "RESULT:"
 ogrinfo -ro -so /vsizip/`basename $file` subdayprecip_output | grep 'H_N'
 
 echo "**************************************************************"
-echo "* subdayprecip-design-csv"
+echo "* d-rain-csv"
 echo "**************************************************************"
 
 file=`curl \
-"https://rain1.fsv.cvut.cz:4433/services/wps?service=wps&version=1.0.0&request=Execute&identifier=subdayprecip-design-csv&datainputs=\[input=${DATA};return_period=${RP};rainlength=${RL};keycolumn=$COL\]" | \
+"https://rain1.fsv.cvut.cz:4433/services/wps?service=wps&version=1.0.0&request=Execute&identifier=d-rain-csv&datainputs=input=${DATA};return_period=${RP};rainlength=${RL};keycolumn=$COL" | \
 grep '\<wps:Reference' | cut -d'"' -f2`
 
 wget -q $file
@@ -34,22 +34,22 @@ echo "RESULT:"
 cat `basename $file` | head -n1
 
 echo "**************************************************************"
-echo "* subdayprecip-design-point"
+echo "* d-rain-point"
 echo "**************************************************************"
 
 value=`curl \
-"https://rain1.fsv.cvut.cz:4433/services/wps?service=wps&version=1.0.0&request=Execute&identifier=subdayprecip-design-point&datainputs=\[obs_x=15.11784;obs_y=49.88598;return_period=${RP};rainlength=${RL}\]" | \
+"https://rain1.fsv.cvut.cz:4433/services/wps?service=wps&version=1.0.0&request=Execute&identifier=d-rain-point&datainputs=obs_x=15.11784;obs_y=49.88598;return_period=${RP};rainlength=${RL}" | \
 grep "\<wps:LiteralData" | cut -d'>' -f 2 | cut -d'<' -f 1`
 
 echo "RESULT:"
 echo $value
 
 echo "**************************************************************"
-echo "* subdayprecip-design-shapes"
+echo "* d-rain-timedist"
 echo "**************************************************************"
 
 file=`curl \
-"https://rain1.fsv.cvut.cz:4433/services/wpspriv?service=wps&version=1.0.0&request=Execute&identifier=subdayprecip-design-shapes&datainputs=\[input=${DATA};return_period=${RP};keycolumn=${COL};type=${STYP}\]" | \
+"https://rain1.fsv.cvut.cz:4433/services/wpspriv?service=wps&version=1.0.0&request=Execute&identifier=d-rain-timedist&datainputs=input=${DATA};return_period=${RP};keycolumn=${COL};type=${STYP}" | \
 grep '\<wps:Reference' | cut -d'"' -f2`
 
 wget -q $file
