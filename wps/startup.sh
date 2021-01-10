ffbe58e44165f7384920927f6290bc6e7d9a44f2
Martin Landa <landa.martin@gmail.com>
2021-01-10 10:52:14 +0100

    wps start: no need for PATH

--- wps/startup.sh
+++ wps/startup.sh
@@ -9,7 +9,6 @@ envsubst '$NGINX_HOST' < /etc/nginx/conf.d/default.conf.template > \
 nginx -g 'daemon off;' &
 
 export LD_LIBRARY_PATH=/usr/lib/grass78/lib
-export PATH=/usr/lib/grass78/bin:/usr/lib/grass78/scripts:$PATH
 
 gunicorn3 -b 127.0.0.1:8081 --workers $((2*`nproc --all`)) \
           --log-syslog  --pythonpath /opt/pywps pywps_app:application
