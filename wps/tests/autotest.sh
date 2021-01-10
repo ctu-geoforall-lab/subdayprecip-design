2f852dfce0f7f332fcb88b195a3fc0387c4813c4
Martin Landa <landa.martin@gmail.com>
2021-01-10 20:29:48 +0100

    request xml -> test

--- wps/tests/autotest.sh
+++ wps/tests/autotest.sh
@@ -11,88 +11,88 @@ STYP="E,F"
 VALUE="25"
 LIMIT="10000"
 
-cp request-d-rain-shp.xml /tmp
+cp tests/request-d-rain-shp.xml /tmp
 cd /tmp
 
-echo "**************************************************************"
-echo "* d-rain-shp (POST) "
-echo "**************************************************************"
-
-wget -q --post-file request-d-rain-shp.xml 'http://localhost:8080/services/wps?' -O response.xml
-file=`cat response.xml | grep '\<wps:Reference' | cut -d'"' -f2`
-wget -q $file
-echo "RESULT:"
-ogrinfo -ro -so /vsizip/`basename $file` subdayprecip_output | grep 'H_N'
-
-echo "**************************************************************"
-echo "* d-rain-shp (GET|XLINK)"
-echo "**************************************************************"
-
-CURL="${URL}/services/wps?service=wps&version=1.0.0&request=Execute&identifier=d-rain-shp&datainputs=input=${DATA};return_period=${RP};rainlength=${RL};area_size=${LIMIT}"
-echo $CURL
-file=`curl "$CURL" |  grep '\<wps:Reference' | cut -d'"' -f2`
-
-wget -q $file
-echo "RESULT:"
-ogrinfo -ro -so /vsizip/`basename $file` subdayprecip_output | grep 'H_N'
-
-echo "**************************************************************"
-echo "* d-rain-csv"
-echo "**************************************************************"
-
-CURL="${URL}/services/wps?service=wps&version=1.0.0&request=Execute&identifier=d-rain-csv&datainputs=input=${DATA};return_period=${RP};rainlength=${RL};keycolumn=$COL;area_size=${LIMIT}"
-echo $CURL
-file=`curl "$CURL" | grep '\<wps:Reference' | cut -d'"' -f2`
-
-wget -q $file
-echo "RESULT:"
-cat `basename $file`
-
-echo "**************************************************************"
-echo "* d-rain-point"
-echo "**************************************************************"
-
-CURL="${URL}/services/wps?service=wps&version=1.0.0&request=Execute&identifier=d-rain-point&datainputs=obs_x=15.11784;obs_y=49.88598;return_period=${RP};rainlength=${RL}"
-echo $CURL
-value=`curl $CURL | grep "\<wps:LiteralData" | cut -d'>' -f 2 | cut -d'<' -f 1`
-
-echo "RESULT:"
-echo $value
-
 # echo "**************************************************************"
-# echo "* d-rain6h-timedist (reduction enabled)"
+# echo "* d-rain-shp (POST) "
 # echo "**************************************************************"
 
-# file=`curl \
-# "${URL}/services/wps?service=wps&version=1.0.0&request=Execute&identifier=d-rain6h-timedist&datainputs=input=${DATA};return_period=${RP};keycolumn=${COL};type=${STYP}" | \
-# grep '\<wps:Reference' | cut -d'"' -f2`
+# wget -q --post-file request-d-rain-shp.xml 'http://localhost:8080/services/wps?' -O response.xml
+# file=`cat response.xml | grep '\<wps:Reference' | cut -d'"' -f2`
+# wget -q $file
+# echo "RESULT:"
+# ogrinfo -ro -so /vsizip/`basename $file` subdayprecip_output | grep 'H_N'
+
+# echo "**************************************************************"
+# echo "* d-rain-shp (GET|XLINK)"
+# echo "**************************************************************"
+
+# CURL="${URL}/services/wps?service=wps&version=1.0.0&request=Execute&identifier=d-rain-shp&datainputs=input=${DATA};return_period=${RP};rainlength=${RL};area_size=${LIMIT}"
+# echo $CURL
+# file=`curl "$CURL" |  grep '\<wps:Reference' | cut -d'"' -f2`
 
 # wget -q $file
 # echo "RESULT:"
-# cat `basename $file` |  grep -E '[0-9],[05],'
+# ogrinfo -ro -so /vsizip/`basename $file` subdayprecip_output | grep 'H_N'
 
 # echo "**************************************************************"
-# echo "* d-rain6h-timedist (reduction disabled)"
+# echo "* d-rain-csv"
 # echo "**************************************************************"
 
-# file=`curl \
-# "${URL}/services/wps?service=wps&version=1.0.0&request=Execute&identifier=d-rain6h-timedist&datainputs=input=${DATA};return_period=${RP};keycolumn=${COL};type=${STYP};area_red=false" | \
-# grep '\<wps:Reference' | cut -d'"' -f2`
+# CURL="${URL}/services/wps?service=wps&version=1.0.0&request=Execute&identifier=d-rain-csv&datainputs=input=${DATA};return_period=${RP};rainlength=${RL};keycolumn=$COL;area_size=${LIMIT}"
+# echo $CURL
+# file=`curl "$CURL" | grep '\<wps:Reference' | cut -d'"' -f2`
 
 # wget -q $file
 # echo "RESULT:"
-# cat `basename $file` |  grep -E '[0-9],[05],'
+# cat `basename $file`
 
 # echo "**************************************************************"
-# echo "* raintotal6h-timedist"
+# echo "* d-rain-point"
 # echo "**************************************************************"
 
-# file=`curl \
-# "${URL}/services/wps?service=wps&version=1.0.0&request=Execute&identifier=raintotal6h-timedist&datainputs=value=${VALUE};type=${STYP}" | \
-# grep '\<wps:Reference' | cut -d'"' -f2`
+# CURL="${URL}/services/wps?service=wps&version=1.0.0&request=Execute&identifier=d-rain-point&datainputs=obs_x=15.11784;obs_y=49.88598;return_period=${RP};rainlength=${RL}"
+# echo $CURL
+# value=`curl $CURL | grep "\<wps:LiteralData" | cut -d'>' -f 2 | cut -d'<' -f 1`
 
-# wget -q $file
 # echo "RESULT:"
-# cat `basename $file` | head -n2
+# echo $value
+
+echo "**************************************************************"
+echo "* d-rain6h-timedist (reduction enabled)"
+echo "**************************************************************"
+
+file=`curl \
+"${URL}/services/wps?service=wps&version=1.0.0&request=Execute&identifier=d-rain6h-timedist&datainputs=input=${DATA};return_period=${RP};keycolumn=${COL};type=${STYP}" | \
+grep '\<wps:Reference' | cut -d'"' -f2`
+
+wget -q $file
+echo "RESULT:"
+cat `basename $file` |  grep -E '[0-9],[05],'
+
+echo "**************************************************************"
+echo "* d-rain6h-timedist (reduction disabled)"
+echo "**************************************************************"
+
+file=`curl \
+"${URL}/services/wps?service=wps&version=1.0.0&request=Execute&identifier=d-rain6h-timedist&datainputs=input=${DATA};return_period=${RP};keycolumn=${COL};type=${STYP};area_red=false" | \
+grep '\<wps:Reference' | cut -d'"' -f2`
+
+wget -q $file
+echo "RESULT:"
+cat `basename $file` |  grep -E '[0-9],[05],'
+
+echo "**************************************************************"
+echo "* raintotal6h-timedist"
+echo "**************************************************************"
+
+file=`curl \
+"${URL}/services/wps?service=wps&version=1.0.0&request=Execute&identifier=raintotal6h-timedist&datainputs=value=${VALUE};type=${STYP}" | \
+grep '\<wps:Reference' | cut -d'"' -f2`
+
+wget -q $file
+echo "RESULT:"
+cat `basename $file` | head -n2
 
 exit 0
