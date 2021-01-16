a86bba2c4e406de40a9b9a6e060b45d70ad9d497
Martin Landa <landa.martin@gmail.com>
2021-01-16 23:11:02 +0100

    ows: wms service implemented

--- ows/startup.sh
+++ ows/startup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# WPS
 envsubst '$NGINX_HOST $NGINX_PORT' < /opt/pywps/pywps.cfg.template > \
          /opt/pywps/pywps.cfg
+# WMS
+envsubst '$NGINX_HOST $NGINX_PORT' < /opt/mapserv/wms/subdayprecip.map.template > \
+         /opt/mapserv/wms/subdayprecip.map
 
 envsubst '$NGINX_HOST' < /etc/nginx/conf.d/default.conf.template > \
          /etc/nginx/conf.d/default.conf
 
-nginx -g 'daemon off;' &
+/etc/init.d/fcgiwrap start && nginx -g 'daemon off;' &
 
 export LD_LIBRARY_PATH=/usr/lib/grass78/lib
 
