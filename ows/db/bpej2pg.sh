1188d009e18d1ae51a2d870ff3e04a19ec24fee8
Martin Landa <landa.martin@gmail.com>
2021-01-18 22:34:15 +0100

    ows: rain_db operational

--- ows/db/bpej2pg.sh
+++ ows/db/bpej2pg.sh
@@ -3,13 +3,11 @@
 # TODO (download automatically)
 FILE=bpej_2021010410908.zip
 URL=https://www.spucr.cz/frontend/webroot/uploads/files/2021/01/$FILE
-DIR=/tmp
+DIR=/data
 DB=bpej
 
-dropdb --if-exists $DB
 createdb $DB
 psql $DB -c 'create extension postgis'
-psql $DB -f epsg5514.sql
 
 cd $DIR
 if [ ! -f "$FILE" ]; then
@@ -18,8 +16,6 @@ fi
 
 ogr2ogr -f PostgreSQL -overwrite -nln bpej -a_srs EPSG:5514 -nlt MULTIPOLYGON \
 	-lco GEOMETRY_NAME=geom \
-        PG:dbname=$DB /vsizip/$DIR/$FILE # BPEJ_20200102
-
-rm -f $FILE
+        PG:dbname=$DB /vsizip/$DIR/$FILE
 
 exit 0
