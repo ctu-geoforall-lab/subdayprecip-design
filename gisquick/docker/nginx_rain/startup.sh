6aff4d7b1348ca8aec43cffc3df8654fccaaf783
Martin Landa <landa.martin@gmail.com>
2021-03-06 23:18:16 +0100

    remove gisquick folder with version 1

--- gisquick/docker/nginx_rain/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-envsubst '$NGINX_HOST' < /etc/nginx/conf.d/gisquick.template > /etc/nginx/conf.d/gisquick.conf && nginx -g 'daemon off;' &
-gunicorn -b 127.0.0.1:8081  --workers $((2*`nproc --all`)) --log-syslog  --pythonpath /opt/subdayprecip-design/wps pywps_app:application
-
-exit 0
