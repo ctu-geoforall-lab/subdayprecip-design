cfd1a7a17af4d5bbb806258675d65afd4ccf162c
Martin Landa <landa.martin@gmail.com>
2021-03-06 11:54:09 +0100

    add NGINX_HTTP variable

--- ows/startup.sh
+++ ows/startup.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
 # WPS
-envsubst '$NGINX_HOST $NGINX_PORT' < /opt/pywps/pywps.cfg.template > \
+envsubst '$NGINX_HTTP $NGINX_HOST $NGINX_PORT' < /opt/pywps/pywps.cfg.template > \
          /opt/pywps/pywps.cfg
 # WMS
 if [ -d /opt/mapserv/wms ] ; then
-    envsubst '$NGINX_HOST $NGINX_PORT' < /opt/mapserv/wms/subdayprecip.map.template > \
+    envsubst '$NGINX_HTTP $NGINX_HOST $NGINX_PORT' < /opt/mapserv/wms/subdayprecip.map.template > \
              /opt/mapserv/wms/subdayprecip.map
 fi
 
 # WFS
 if [ -d /opt/mapserv/wfs ] ; then
-    envsubst '$NGINX_HOST $NGINX_PORT $MAPSERV_USER $MAPSERV_PASSWORD $DBNAME' \
+    envsubst '$NGINX_HTTP $NGINX_HOST $NGINX_PORT $MAPSERV_USER $MAPSERV_PASSWORD $DBNAME' \
              < /opt/mapserv/wfs/subdayprecip.map.template > \
              /opt/mapserv/wfs/subdayprecip.map
 fi
 
+# d-rain-point
+envsubst '$NGINX_HTTP $NGINX_HOST $NGINX_PORT' \
+         < /var/www/html/d-rain-point/main.js.template > \
+         /var/www/html/d-rain-point/main.js
+
+# nginx
 envsubst '$NGINX_HOST' < /etc/nginx/conf.d/default.conf.template > \
          /etc/nginx/conf.d/default.conf
 
+# start nginx
 /etc/init.d/fcgiwrap start && nginx -g 'daemon off;' &
 
 export LD_LIBRARY_PATH=/usr/lib/grass78/lib
